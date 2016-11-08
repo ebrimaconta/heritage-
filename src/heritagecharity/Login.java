2f591c66b98395706178f72b2cfe9e4f40f490f8
Hussein <tejanahmedhu@gmail.com>
2016-11-08 00:08:39 +0000

    added login form

---
+++ src/heritagecharity/Login.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heritagecharity;
+
+/**
+ *
+ * 
+ */
+public class Login {
+    
+    String user;
+    String pass;
+    
+    
+    public Login(String username, String password){
+        username = user;
+        password = pass;
+        
+        
+    }
+    
+   public String getUserName(){
+       return user;
+   }
+   
+    public void setUserName(String username){
+       user=username;
+   }
+    
+    public String getPassword(){
+       return pass;
+   }
+   
+    public void setSurame(String password){
+       pass=password;
+   }
+    
+    
+}
